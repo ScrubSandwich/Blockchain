25e169c5de2b93e8b9f7caa8975df7929c12cc45
jmieczni <jmieczni@purdue.edu>
2018-07-28 17:54:06 -0400

    Add Block

---
+++ Block.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class Block {
+  private String hash;
+  private String previousHash;
+  private String data;
+  private long timestamp;
+
+  public Block(String data, String previousHash) {
+    this.data = data;
+    this.previousHash;
+    this.timestamp = new Date().getTime();
+  }
+}
